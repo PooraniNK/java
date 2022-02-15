734f6b4b66d71bcc5d167467c54bf2053b3611dd
PooraniNK <99708741+PooraniNK@users.noreply.github.com>
2022-02-15 19:47:08 +0530

    Update App.java

--- src/main/java/com/acn/app/App.java
+++ src/main/java/com/acn/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Today" );
+        System.out.println( "Hello World Todays" );
     }
 
 	public static String getHelloWorld() {
